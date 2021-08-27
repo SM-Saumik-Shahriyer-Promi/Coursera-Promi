57392f5e60175fe94850eca6a2ecabd6c2ce0a19
SM-Saumik-Shahriyer-Promi <saumik.ruet.ete15@gmail.com>
2021-08-28 02:04:04 +0600

    update

--- Module4Assignment/script.js
+++ Module4Assignment/script.js
@@ -3,17 +3,17 @@ names[0]="Yaakov";
 names[1]="John";
 names[2]="Jen";
 names[3]="jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
-names[9]="jim";
+names[4]="Paul";
+names[5]="Frank";
+names[6]="Larry";
+names[7]="Paula";
+names[8]="Laura";
+names[9]="Jim";
 
 
 for (var x = 0; x < names.length; x++) {
 	if(names[x].charAt(0)==='J'|| names[x].charAt(0)==='j'){
-        console.log("Goodbye "+ names[x])
+        console.log("Good Bye "+ names[x])
 	}
 	else{
 		console.log("Hello "+ names[x])
